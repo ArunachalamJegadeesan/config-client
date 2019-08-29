547ba4b700d8a9680b3da33ff2d68a9584991357
ArunachalamJegadeesan <inboxofarun@gmail.com>
2019-08-30 00:07:57 +0530

    first commit

---
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,34 @@
+package com.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
+
+@RestController
+class TestRestController{
+
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+
+	@RequestMapping(method= RequestMethod.GET,value="welcome" )
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	public String getMessage() {
+		return welcomeMessage;
+  }
+	
+
+}
